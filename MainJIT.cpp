62d7c89c0cfdc08222736f304fe8239056ebd282
hanatok <summersnow9403@gmail.com>
2020-12-12 20:15:25 -0600

    WIP: JIT
    The LLVM JIT tutorial is hard to read since it is going to change the
    API from ORCv1 to ORCv2. Anyway, this code compiles, but I have not
    checked if it actually works yet.

--- MainJIT.cpp
+++ MainJIT.cpp
@@ -1,14 +1,73 @@
 #include "MainJIT.h"
 
+namespace llvm {
+namespace orc {
+
+// Constructor
+// SectionMemoryManager: an off-the-shelf utility that provides all the basic 
+//                       memory management functionality.
+// ConcurrentIRCompiler: build LLVM target machine
 MainJIT::MainJIT(JITTargetMachineBuilder JTMB, DataLayout DL):
+  mTM(EngineBuilder().selectTarget()), mDL(move(DL)),
   mObjectLayer(mES, [](){return make_unique<SectionMemoryManager>();}),
   mCompileLayer(mES, mObjectLayer,
                 make_unique<ConcurrentIRCompiler>(move(JTMB))),
-  mDL(move(DL)), mMangle(mES, mDL), mCtx(make_unique<LLVMContext>()),
-  mMainJD(mES.createBareJITDylib("<main>")){
-  using namespace llvm;
-  using namespace llvm::orc;
+  mOptimizeLayer(mES, mCompileLayer, optimizeModule),
+  mCompileCallbackManager(move(createLocalCompileCallbackManager(
+    mTM->getTargetTriple(), mES, 0).get())),
+  mMangle(mES, mDL), mCtx(make_unique<LLVMContext>()),
+  mMainJD(mES.createBareJITDylib("<main>")) {
+  // mMainJD is a dylib that contains not only the symbols we add, but also
+  // from runtime
   mMainJD.addGenerator(
     cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(
       mDL.getGlobalPrefix())));
 }
+
+Expected<unique_ptr<MainJIT>> MainJIT::Create() {
+  auto JTMB = JITTargetMachineBuilder::detectHost();
+  if (!JTMB)
+    return JTMB.takeError();
+  auto DL = JTMB->getDefaultDataLayoutForTarget();
+  if (!DL)
+    return DL.takeError();
+  return make_unique<MainJIT>(move(*JTMB), move(*DL));
+}
+
+const DataLayout& MainJIT::getDataLayout() const {
+  return mDL;
+}
+
+LLVMContext& MainJIT::getContext() {
+  return *mCtx.getContext();
+}
+
+Error MainJIT::addModule(unique_ptr<Module> M) {
+  return mOptimizeLayer.add(mMainJD, ThreadSafeModule(move(M), mCtx));
+}
+
+Expected<JITEvaluatedSymbol> MainJIT::lookup(StringRef Name) {
+  return mES.lookup({&mMainJD}, mMangle(Name.str()));
+}
+
+Expected<ThreadSafeModule> MainJIT::optimizeModule(ThreadSafeModule TSM,
+                                          const MaterializationResponsibility &R) {
+  TSM.withModuleDo([](Module &M) {
+    // create a function pass manager
+    auto FPM = make_unique<legacy::FunctionPassManager>(&M);
+    // add some optimizations
+    FPM->add(createInstructionCombiningPass());
+    FPM->add(createReassociatePass());
+    FPM->add(createGVNPass());
+    FPM->add(createCFGSimplificationPass());
+    FPM->doInitialization();
+    // Run the optimizations over all functions in the module being added to
+    // the JIT.
+    for (auto &F : M)
+      FPM->run(F);
+  });
+  return move(TSM);
+}
+
+} // end namespace orc
+} // end namespace llvm
--- MainJIT.h
+++ MainJIT.h
@@ -1,46 +1,82 @@
 #ifndef MAINJIT_H
 #define MAINJIT_H
 
-#include <llvm/ADT/StringRef.h>
+#include <llvm/ADT/STLExtras.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
 #include <llvm/ExecutionEngine/JITSymbol.h>
-#include <llvm/ExecutionEngine/Orc/CompileUtils.h>
-#include <llvm/ExecutionEngine/Orc/Core.h>
+#include <llvm/ExecutionEngine/Orc/CompileOnDemandLayer.h>
 #include <llvm/ExecutionEngine/Orc/ExecutionUtils.h>
+#include <llvm/ExecutionEngine/Orc/CompileUtils.h>
 #include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
-#include <llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h>
+#include <llvm/ExecutionEngine/Orc/IRTransformLayer.h>
+#include <llvm/ExecutionEngine/Orc/LambdaResolver.h>
 #include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#include <llvm/ExecutionEngine/RTDyldMemoryManager.h>
+#include <llvm/ExecutionEngine/RuntimeDyld.h>
 #include <llvm/ExecutionEngine/SectionMemoryManager.h>
-#include <llvm/ExecutionEngine/Orc/ThreadSafeModule.h>
 #include <llvm/IR/DataLayout.h>
-#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Mangler.h>
+#include <llvm/Support/DynamicLibrary.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Transforms/InstCombine/InstCombine.h>
+#include <llvm/Transforms/Scalar.h>
+#include <llvm/Transforms/Scalar/GVN.h>
 #include <memory>
-
-using llvm::orc::ExecutionSession;
-using llvm::orc::RTDyldObjectLinkingLayer;
-using llvm::orc::IRCompileLayer;
-using llvm::DataLayout;
-using llvm::orc::ThreadSafeContext;
-using llvm::orc::JITTargetMachineBuilder;
-using llvm::orc::ConcurrentIRCompiler;
-using llvm::orc::MangleAndInterner;
-using llvm::SectionMemoryManager;
-using llvm::LLVMContext;
-using llvm::orc::JITDylib;
+#include <map>
 
 using std::make_unique;
+using std::make_shared;
 using std::move;
+using std::unique_ptr;
+using std::string;
+using std::map;
+using std::shared_ptr;
 
+namespace llvm {
+namespace orc {
+
+// The JIT code looks cryptic and what the tutorial says does not always
+// match the full code list.
+// LLVM 12 will significantly change the ORC API, and the following code
+// should be adapted accordingly.
 class MainJIT {
 private:
+  // provide context for JIT'd code and represent the JIT'd program
   ExecutionSession mES;
+  unique_ptr<TargetMachine> mTM;
+  const DataLayout mDL; // symbol mangling
+  // object linking layer
   RTDyldObjectLinkingLayer mObjectLayer;
+
+  // LLVM IR layer
   IRCompileLayer mCompileLayer;
-  DataLayout mDL;
-  MangleAndInterner mMangle;
-  ThreadSafeContext mCtx;
+  // optimization layer
+  IRTransformLayer mOptimizeLayer;
+
+  unique_ptr<JITCompileCallbackManager> mCompileCallbackManager;
+//   CompileOnDemandLayer mCODLayer;
+
+  MangleAndInterner mMangle; // symbol mangling
+  ThreadSafeContext mCtx; // LLVMContext for building IR files
+  // provide the symbol table
   JITDylib &mMainJD;
 public:
+//   MainJIT();
   MainJIT(JITTargetMachineBuilder JTMB, DataLayout DL);
+  static Expected<unique_ptr<MainJIT>> Create();
+  const DataLayout &getDataLayout() const;
+  LLVMContext &getContext();
+  Error addModule(unique_ptr<Module> M);
+  Expected<JITEvaluatedSymbol> lookup(StringRef Name);
+private:
+//   string mangle(const string& Name);
+  static Expected<ThreadSafeModule>
+  optimizeModule(ThreadSafeModule TSM, const MaterializationResponsibility &R);
 };
+
+} // end namespace orc
+} // end namespace llvm
 
 #endif // MAINJIT_H
